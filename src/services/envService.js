b859bb9a1e6efe590ccc9de82820970ad4596304
TheTrunk <tadeas.kmenta@zel.cash>
2022-12-10 08:53:42 +0700

    fixes

--- src/services/envService.js
+++ src/services/envService.js
@@ -1,57 +1,49 @@
 const config = require('config');
-const log = require('../lib/log');
 
 const serviceHelper = require('./serviceHelper');
 
 async function getEnv(id) {
-  try {
-    const db = serviceHelper.databaseConnection();
-    const database = db.db(config.database.database);
-    const envCollection = config.collections.env;
-    const query = { envid: id };
-    const projection = {
-      projection: {
-        _id: 0,
-        env: 1,
-        envid: 1,
-      },
-    };
-    const envRes = await serviceHelper.findOneInDatabase(database, envCollection, query, projection);
-    if (envRes) {
-      return envRes.env;
-    }
-    throw new Error(`ENV ${id} not found`);
-  } catch (error) {
-    log.error(error);
-    return null;
+  const db = serviceHelper.databaseConnection();
+  const database = db.db(config.database.database);
+  const envCollection = config.collections.env;
+  const query = { envid: id };
+  const projection = {
+    projection: {
+      _id: 0,
+      env: 1,
+      envid: 1,
+    },
+  };
+  const envRes = await serviceHelper.findOneInDatabase(database, envCollection, query, projection);
+  if (envRes) {
+    return envRes.env;
   }
+  throw new Error(`ENV ${id} not found`);
 }
 
 // data is an object of envid, env
 async function postEnv(data) {
-  try {
-    const db = serviceHelper.databaseConnection();
-    const database = db.db(config.database.database);
-    const envCollection = config.collections.env;
-    const query = { envid: data.envid };
-    const projection = {
-      projection: {
-        _id: 0,
-        env: 1,
-        envid: 1,
-      },
-    };
-    const envExists = await serviceHelper.findOneInDatabase(database, envCollection, query, projection);
-    if (envExists) {
-      throw new Error(`ENV of ${data.envid} already exists, can't be updated`);
-    }
-    // insert to database
-    await serviceHelper.insertOneToDatabase(database, envCollection, data);
-    return data; // all ok
-  } catch (error) {
-    log.error(error);
-    return null;
+  const db = serviceHelper.databaseConnection();
+  const database = db.db(config.database.database);
+  const envCollection = config.collections.env;
+  const query = { envid: data.envid };
+  const timestamp = new Date().getTime();
+  // eslint-disable-next-line no-param-reassign
+  data.timestamp = timestamp;
+  const projection = {
+    projection: {
+      _id: 0,
+      env: 1,
+      envid: 1,
+    },
+  };
+  const envExists = await serviceHelper.findOneInDatabase(database, envCollection, query, projection);
+  if (envExists) {
+    throw new Error(`ENV of ${data.envid} already exists, can't be updated`);
   }
+  // insert to database
+  await serviceHelper.insertOneToDatabase(database, envCollection, data);
+  return data; // all ok
 }
 
 module.exports = {
